357427112068179993823092678ed6470088883f
ogola5 <ogolaevance5@gmail.com>
2022-09-19 12:34:35 +0300

    updated

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -2,10 +2,10 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Header from "../components/Header";
+import Header from "../components/Header.test.js!";
 
 test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+  render(<Header />)
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
