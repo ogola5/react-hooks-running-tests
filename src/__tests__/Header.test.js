16fd977f30781d9909040552c9c37a6b3e3f34f5
ogola5 <ogolaevance5@gmail.com>
2022-09-27 23:59:35 +0300

    completd assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -2,11 +2,11 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
-<<<<<<< HEAD
+
 import Header from '../components/Header'
-=======
+
 import Header from "../components/Header.test.js!";
->>>>>>> 357427112068179993823092678ed6470088883f
+
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />)
